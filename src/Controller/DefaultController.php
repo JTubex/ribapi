684578afbf41c771368bc985d107ceba8dca00e6
Jeroen Tubex <JeroenT@2228934.no-reply.drupal.org>
2018-11-22 21:08:37 +0100

    Add led test

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -9,4 +9,13 @@ class DefaultController extends AbstractController {
   public function index() {
     return $this->render('index/index.html.twig');
   }
+
+  public function ledOn() {
+    return $this->redirectToRoute('index');
+  }
+
+  public function ledOff() {
+    return $this->redirectToRoute('index');
+  }
+
 }
